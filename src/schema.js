e87b8f32b8e9a7df9b3ffca8480d34ea791b3295
Naab14 <abbe.nasr@icloud.com>
2026-06-18 06:04:14 +0200

    fix(review 4): drop redundant diacritic replacements

--- src/schema.js
+++ src/schema.js
@@ -78,9 +78,6 @@ export function processIdForName(name) {
     .normalize('NFD')
     .replace(/[\u0300-\u036f]/g, '')
     .toLowerCase()
-    .replace(/å/g, 'a')
-    .replace(/ä/g, 'a')
-    .replace(/ö/g, 'o')
     .replace(/[^a-z0-9]+/g, '-')
     .replace(/^-+|-+$/g, '') || 'process';
 }
